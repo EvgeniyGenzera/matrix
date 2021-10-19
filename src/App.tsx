69943872740cc22cbbaaec4dc8392bf61fe24099
unknown <fkca.av15.gev@gmail.com>
2021-10-19 22:00:12 +0300

    pull to home computer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Navbar } from './components/navbar';
 import { Builder } from './components/builder';
 import { Matrix } from './components/matrix';
 
-function App() {
-  return (
-    <>
-    <Navbar />
-    <Builder />
-    <Matrix />
-    </>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<>
+			<Navbar />
+			<Builder />
+			<Matrix />
+		</>
+	);
+};
 
 export default App;
--- src/components/matrix.tsx
+++ src/components/matrix.tsx
@@ -3,53 +3,7 @@ import React from 'react';
 export const Matrix: React.FC = () => (
 	<>
 		<button className="matrix-btn">Add row</button>
-		{/* <div className="matrix-table">
-			<div className="matrix-head">
-				<ul>
-					<li>№</li>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
-					<li>4</li>
-					<li>Sum</li>
-				</ul>
-			</div>
-			<div className="matrix-container">
-				<ul className="first-column">
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
-					<li>4</li>
-					<li>Avg</li>
-				</ul>
-				<ul className="matrix-content">
-					<li>543</li>
-					<li>231</li>
-					<li>321</li>
-					<li>231</li>
-					<li>432</li>
-					<li>767</li>
-					<li>566</li>
-					<li>322</li>
-					<li>344</li>
-					<li>222</li>
-					<li>111</li>
-					<li>333</li>
-					<li>444</li>
-					<li>555</li>
-					<li>666</li>
-					<li>777</li>
-				</ul>
-				<ul className="sum-column">
-					<li>333</li>
-					<li>444</li>
-					<li>555</li>
-					<li>666</li>
-					<li>777</li>
-				</ul>
-			</div>
-		</div> */}
-		<div className='matrix'>
+		<div className="matrix">
 			<ul>
 				<li>№</li>
 				<li>1</li>
@@ -66,7 +20,7 @@ export const Matrix: React.FC = () => (
 				<li>653</li>
 				<li>764</li>
 			</ul>
-      <ul>
+			<ul>
 				<li>2</li>
 				<li>642</li>
 				<li>233</li>
@@ -74,7 +28,7 @@ export const Matrix: React.FC = () => (
 				<li>653</li>
 				<li>764</li>
 			</ul>
-      <ul>
+			<ul>
 				<li>3</li>
 				<li>642</li>
 				<li>233</li>
@@ -82,7 +36,7 @@ export const Matrix: React.FC = () => (
 				<li>653</li>
 				<li>764</li>
 			</ul>
-      <ul>
+			<ul>
 				<li>Sum</li>
 				<li>642</li>
 				<li>233</li>
