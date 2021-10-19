5ef628954769b9ab8f51c4e7915ed1f9759561cf
EvgeniyGenzera <fkca.av15.gev@gmail.com>
2021-10-19 21:19:51 +0300

    add markup

---
+++ src/components/builder.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Builder: React.FC = () => (
+  <div className='builder lighten-2 card-panel teal'>
+    <h1 className='title yellow-text'>Matrix builder</h1>
+    <form className='form'>
+      <p className='fields white-text'>Enter the number of columns <input className='white' type='number' /></p>
+      <p className='fields white-text'>Enter the number of rows <input className='white' type='number' /></p>
+      <p className='fields white-text'>Enter the number of cells <input className='white' type='number' /></p>
+      <button className='btn red-text'>Create Matrix</button>
+    </form>
+  </div>
+)
